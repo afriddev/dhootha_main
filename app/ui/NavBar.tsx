a8b12dfcc8c1b5c72915f5baeda9c6af306f4ac3
SHIAK AFRID <afridayan01@gmail.com>
2024-06-07 04:13:13 +0000

    fixed mobile view nav bar visibility

--- app/ui/NavBar.tsx
+++ app/ui/NavBar.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import { HiOutlineMenuAlt4 } from "react-icons/hi";
 import {
   ABOUT_US,
@@ -13,24 +13,23 @@ import Link from "next/link";
 import { useState } from "react";
 
 function NavBar() {
-  const [open,setOpen] = useState(false)
-  
-  function handleMenu(){
-    setOpen(true)
+  const [open, setOpen] = useState(false);
 
+  function handleMenu() {
+    setOpen(true);
   }
   return (
-    <div className="relative flex lg:h-24  w-full items-center bg-red-400  ">
-      <HiOutlineMenuAlt4 onClick={handleMenu} className="absolute right-6 float-right block h-7 w-7  cursor-pointer lg:hidden" />
-      <Link
-        href={"/"}
-        className="absolute left-4 w-20 text-wrap lg:left-8  lg:w-fit "
-      >
-        <label className=" flex items-center gap-x-1 text-xl font-medium lg:cursor-pointer lg:text-3xl lg:hover:scale-105">
-          {DOOTHA_PROMOTIONS}
-        </label>
-      </Link>
-      <div className="absolute right-10 hidden lg:block">
+    <div className=" flex h-fit  items-center bg-secondary  ">
+      <div className=" w-full items-center justify-between flex pl-4 pr-5 py-2 lg:py-4">
+        <Link href={"/"}>
+          <label className="w-1/3 lg:w-full flex items-center gap-x-1 text-2xl font-medium lg:cursor-pointer lg:text-3xl lg:hover:scale-105">
+            {DOOTHA_PROMOTIONS}
+          </label>
+        </Link>
+        <HiOutlineMenuAlt4 className="h-7 w-7 block lg:hidden" onClick={handleMenu} />
+
+      </div>
+      <div className=" hidden lg:block lg:pr-6">
         <li className="flex list-none items-center gap-x-10">
           <Link href={"/portfolio"}>
             <Button variant={"outline"}>{PORTFOLIO}</Button>
@@ -42,12 +41,8 @@ function NavBar() {
             <Button variant={"outline"}>{CONTACT}</Button>
           </Link>
           <Link href={"/home"}>
-          <Button
-           
-            className="hidden lg:block"
-          >
-            {HOME}
-          </Button></Link>
+            <Button className="hidden lg:block">{HOME}</Button>
+          </Link>
         </li>
       </div>
     </div>
