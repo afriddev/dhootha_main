a9a216e546cbbf99525e30cac303bbf18dca7e6b
afriddev <afridayan01@gmail.com>
2024-12-12 16:24:06 +0000

    added  all vite files

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import AppContext from "./utils/AppContext.tsx";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <AppContext>
+      <App />
+    </AppContext>
+  </StrictMode>
+);
