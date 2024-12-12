3c5530c74cbd88317ec6eff63c9c064dc13e3e4e
afriddev <afridayan01@gmail.com>
2024-12-12 16:23:02 +0000

    removed old fIles

--- app/ui/Spinner.tsx
+++
@@ -1,8 +0,0 @@
-function Spinner(){
-    return (
-        <div className="flex fixed bg-black/60 w-screen h-screen items-center justify-center z-[999]" >
-            <div className="loader"></div>
-        </div>
-    )
-}
-export default Spinner
